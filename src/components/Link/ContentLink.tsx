7b57be9867e84ab4f14d53a66dc625d4fe713a5d
davidcetinkaya <david.o.cetinkaya@gmail.com>
2021-03-14 21:18:07 +0100

    Remove border on ContentLink and make it inline-block

--- src/components/Link/ContentLink.tsx
+++ src/components/Link/ContentLink.tsx
@@ -2,22 +2,10 @@ import React from 'react'
 import styled from 'styled-components'
 import { PlainLink, PropType } from './PlainLink'
 import { gradientTextStyles } from 'utils'
-import { supportsStyles } from 'consts'
 
 const Wrapper = styled(PlainLink)`
   ${gradientTextStyles};
-  border-bottom: 0.1rem solid var(--brand-primary);
-  ${supportsStyles.gradientText} {
-    border-image: linear-gradient(
-        90deg,
-        var(--brand-primary),
-        var(--brand-secondary)
-      )
-      20%;
-  }
-  border-top: 0;
-  border-left: 0;
-  border-right: 0;
+  display: inline-block;
 `
 
 export const ContentLink = (props: PropType) => {
